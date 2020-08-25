ffc8b62fa7da1a078c859fa2b0f9c76ec4a0a3a2
s9abriel <s9abriel@163.com>
2020-08-25 15:38:33 +0800

    测试ribbon负载均衡

--- cloud-consumer-order80/src/main/java/com/s9abriel/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/s9abriel/springcloud/controller/OrderController.java
@@ -4,6 +4,7 @@ package com.s9abriel.springcloud.controller;
 import com.s9abriel.springcloud.entities.CommonResult;
 import com.s9abriel.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +34,13 @@ public class OrderController {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
     }
 
+    @GetMapping("consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "payment/get/"+id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        }else {
+            return new CommonResult<>(444,"操作失败");
+        }
+        }
 }
